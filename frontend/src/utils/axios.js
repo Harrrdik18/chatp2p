67a12a60ea96002d89395ad78012a260c3f63f44
Harrrdik18.git <patelhardik999666@gmail.com>
2025-02-27 18:36:53 +0530

    added backend link

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,15 +1,15 @@
-import axios from 'axios';
+import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: "https://chatp2p.onrender.com",
   headers: {
-    'Content-Type': 'application/json'
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 instance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -20,4 +20,4 @@ instance.interceptors.request.use(
   }
 );
 
-export default instance; 
+export default instance;
